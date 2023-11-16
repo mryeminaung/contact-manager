b06b19265d5a5f2a2278c258d5c79d1b48dd452d
Ye Min Aung <yeminaung013@gmail.com>
2023-11-16 11:34:57 +0630

    tailwind config and libs setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 const App = () => {
-	return <h1>Contact Manager</h1>;
+	return (
+		<div className="flex items-center justify-center h-screen">
+			<h1 className="text-3xl">Contact Manager</h1>
+		</div>
+	);
 };
 
 export default App;
